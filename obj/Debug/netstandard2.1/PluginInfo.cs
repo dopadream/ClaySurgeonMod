e3ab8c2b04d87faff81887e4ce757448786c42f7
dopadream <slyshidiscord@gmail.com>
2024-11-10 00:39:57 -0600

    init

---
+++ obj/Debug/netstandard2.1/PluginInfo.cs
@@ -0,0 +1,9 @@
+namespace ClaySurgeonMod
+{
+    public static class PluginInfo
+    {
+        public const string PLUGIN_GUID = "ClaySurgeonMod";
+        public const string PLUGIN_NAME = "ClaySurgeonMod";
+        public const string PLUGIN_VERSION = "1.0.0";
+    }
+}
