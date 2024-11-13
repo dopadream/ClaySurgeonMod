a4151c3ed13b5c6fd605fd6b1e1f7261cbee3fe3
dopadream <slyshidiscord@gmail.com>
2024-11-12 18:38:07 -0600

    more

--- Plugin.cs
+++ Plugin.cs
@@ -13,6 +13,8 @@ using LethalConfig;
 using BepInEx.Configuration;
 using LethalConfig.ConfigItems.Options;
 using LethalConfig.ConfigItems;
+using static UnityEngine.UIElements.UIR.Implementation.UIRStylePainter;
+using BarberFixes;
 
 namespace ClaySurgeonMod
 {
@@ -27,8 +29,12 @@ namespace ClaySurgeonMod
         internal static GameObject clayPrefab;
         internal static GameObject barberPrefab;
         internal static TerminalNode clayNode;
+        internal static EnemyType dummyType;
+        internal static EnemyType curveDummyType;
         internal static Dictionary<string, EnemyType> allEnemiesList = [];
         internal static ConfigEntry<bool> configSpawnOverride;
+        internal static ConfigEntry<bool> configInfestations;
+        internal static ConfigEntry<bool> configCurve;
         internal static ConfigEntry<float> configAmbience;
         internal static ConfigEntry<float> configIridescence;
         protected const string anchorPath = "MeshContainer";
@@ -41,10 +47,16 @@ namespace ClaySurgeonMod
 
 
 
-            configSpawnOverride = Config.Bind("General", "Override spawn settings", true,
-                new ConfigDescription("Override (force enable) the spawn in pair functionality from BarberFixes."));
+            configSpawnOverride = Config.Bind("General", "Override Spawn Settings", true,
+                new ConfigDescription("Overrides spawning logic of Clay Surgeons (Barbers). With this enabled, they will spawn in pairs and be more common. Disable if you want to customize their spawning yourself through plugins such as LethalQuantities."));
 
-            configAmbience = Config.Bind("Aesthetics", "Proximity Ambience Volume", 0.5f,
+            configInfestations = Config.Bind("General", "Clay Infestations", true,
+                new ConfigDescription("Adds a chance for Hoarding bug/Nutcracker infestations to be Clay Surgeon Infestations instead."));
+
+            configCurve = Config.Bind("General", "Curved Acceleration", true,
+                new ConfigDescription("Makes the Clay Surgeon accelerate at a curve rather than a flat rate per hour."));
+
+            configAmbience = Config.Bind("Aesthetics", "Proximity Ambience Volume", 0.35f,
                 new ConfigDescription(
                     "Controls the volume of the Clay Surgeon's proximity ambience.",
                     new AcceptableValueRange<float>(0.0f, 1.0f)));
@@ -56,6 +68,8 @@ namespace ClaySurgeonMod
 
 
             LethalConfigManager.AddConfigItem(new BoolCheckBoxConfigItem(configSpawnOverride, false));
+            LethalConfigManager.AddConfigItem(new BoolCheckBoxConfigItem(configInfestations, false));
+            LethalConfigManager.AddConfigItem(new BoolCheckBoxConfigItem(configCurve, false));
             LethalConfigManager.AddConfigItem(new FloatSliderConfigItem(configAmbience, false));
             LethalConfigManager.AddConfigItem(new FloatSliderConfigItem(configIridescence, false));
 
@@ -69,6 +83,9 @@ namespace ClaySurgeonMod
                 AssetBundle claysurgeonbundle = AssetBundle.LoadFromFile(Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "claysurgeonmod"));
                 clayPrefab = claysurgeonbundle.LoadAsset("ClaySurgeonNew", typeof(GameObject)) as GameObject;
                 clayNode = claysurgeonbundle.LoadAsset("ClaySurgeonFile", typeof(TerminalNode)) as TerminalNode;
+                dummyType = claysurgeonbundle.LoadAsset("DummyEnemyType", typeof(EnemyType)) as EnemyType;
+                curveDummyType = claysurgeonbundle.LoadAsset("CurveDummyType", typeof(EnemyType)) as EnemyType;
+
             }
             catch
             {
@@ -83,8 +100,37 @@ namespace ClaySurgeonMod
         }
 
         [HarmonyPatch]
-        class BarberMaterialTweaksPatches
+        class ClaySurgeonPatches
         {
+
+            [HarmonyPatch(typeof(RoundManager), "RefreshEnemiesList")]
+            [HarmonyPostfix]
+            static void RoundManagerPostRefresh(RoundManager __instance, ref int ___enemyRushIndex)
+            {
+                DateTime dateTime = new DateTime(DateTime.Now.Year, 10, 31);
+                bool num = DateTime.Today == dateTime;
+                System.Random random2 = new System.Random(StartOfRound.Instance.randomMapSeed + 5781);
+                if ((num && random2.Next(0, 210) < 3) || random2.Next(0, 1000) < 15)
+                {
+                    if (___enemyRushIndex == -1 && configInfestations.Value)
+                    {
+                        for (int j = 0; j < __instance.currentLevel.Enemies.Count; j++)
+                        {
+                            if (__instance.currentLevel.Enemies[j].enemyType.enemyName == "Clay Surgeon")
+                            {
+                                if (__instance.currentLevel.Enemies[j].enemyType.MaxCount > 0)
+                                {
+                                    Logger.LogDebug("Clay infestation started!");
+                                    ((Component)(object)__instance.indoorFog).gameObject.SetActive(random2.Next(0, 100) < 20);
+                                    ___enemyRushIndex = j;
+                                    __instance.currentMaxInsidePower = 30f;
+                                    break;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
 
             [HarmonyPatch(typeof(QuickMenuManager), "Start")]
             [HarmonyPostfix]
@@ -101,16 +147,28 @@ namespace ClaySurgeonMod
                 if (configSpawnOverride.Value)
                 {
                     foreach (List<SpawnableEnemyWithRarity> enemies in allEnemyLists)
-                    {
                         foreach (SpawnableEnemyWithRarity spawnableEnemyWithRarity in enemies)
                         {
-                            if (allEnemiesList.ContainsKey(spawnableEnemyWithRarity.enemyType.name) && spawnableEnemyWithRarity.enemyType.name == "Clay Surgeon")
+                            if (allEnemiesList.ContainsKey(spawnableEnemyWithRarity.enemyType.name))
+                            {
+                                if (allEnemiesList[spawnableEnemyWithRarity.enemyType.name] == spawnableEnemyWithRarity.enemyType)
+                                    Plugin.Logger.LogWarning($"allEnemiesList: Tried to cache reference to \"{spawnableEnemyWithRarity.enemyType.name}\" more than once");
+                                else
+                                    Plugin.Logger.LogWarning($"allEnemiesList: Tried to cache two different enemies by same name ({spawnableEnemyWithRarity.enemyType.name})");
+                            }
+                            else if (spawnableEnemyWithRarity.enemyType.enemyName == dummyType.enemyName)
                             {
                                 spawnableEnemyWithRarity.enemyType.spawnInGroupsOf = 2;
                                 spawnableEnemyWithRarity.enemyType.MaxCount = 8;
+                                spawnableEnemyWithRarity.enemyType.probabilityCurve = dummyType.probabilityCurve;
+                                spawnableEnemyWithRarity.enemyType.numberSpawnedFalloff = dummyType.numberSpawnedFalloff;
+                                spawnableEnemyWithRarity.enemyType.useNumberSpawnedFalloff = true;
+                                spawnableEnemyWithRarity.enemyType.numberSpawned = 0;
+                                Logger.LogDebug("Barber spawn settings overriden!");
+                                return;
                             }
                         }
-                    }
+
                 }
             }
 
@@ -126,6 +184,7 @@ namespace ClaySurgeonMod
                 __instance.skin = clayClone.GetComponentInChildren<SkinnedMeshRenderer>();
                 __instance.gameObject.GetComponentInChildren<ScanNodeProperties>().headerText = "Clay Surgeon";
                 clayClone.GetComponentInChildren<EnemyAnimationEvent>().mainScript = __instance.gameObject.GetComponentInChildren<EnemyAnimationEvent>().mainScript;
+
             }
 
             [HarmonyPatch(typeof(ClaySurgeonAI), "Start")]
@@ -145,8 +204,9 @@ namespace ClaySurgeonMod
 
             static void ClaySurgeonAIPostUpdate(ClaySurgeonAI __instance)
             {
-               AudioSource[] sources = __instance.gameObject.GetComponentsInChildren<AudioSource>();
-               foreach (AudioSource source in sources)
+                Logger.LogDebug("Speed: " + __instance.currentInterval);
+                AudioSource[] sources = __instance.gameObject.GetComponentsInChildren<AudioSource>();
+                foreach (AudioSource source in sources)
                 {
                     if (source.clip.name == "ClaySurgeonAmbience")
                     {
@@ -154,7 +214,6 @@ namespace ClaySurgeonMod
                         return;
                     }
                 }
-
             }
 
             [HarmonyPatch(typeof(ClaySurgeonAI), "SetVisibility")]
@@ -169,6 +228,25 @@ namespace ClaySurgeonMod
                     barberMat.SetFloat("_AlphaCutoff", (num - __instance.minDistance) / (__instance.maxDistance - __instance.minDistance));
                 __instance.skin.material.SetFloat("_IridescenceMask", configIridescence.Value);
                 __instance.skin.sharedMaterials = barberMats;
+            }
+
+
+            [HarmonyPatch(typeof(DanceClock), nameof(DanceClock.Tick))]
+            [HarmonyPrefix]
+            static bool DanceClockPreTick()
+            {
+                if (configCurve.Value)
+                {
+                    float currentInterval = Mathf.Clamp(curveDummyType.probabilityCurve.Evaluate((float)TimeOfDay.Instance.hour / TimeOfDay.Instance.numberOfHours), 1.25f, 2.75f);
+                    foreach (ClaySurgeonAI barber in UnityEngine.Object.FindObjectsOfType<ClaySurgeonAI>())
+                        barber.currentInterval = currentInterval;
+
+                    // skip the original function
+                    return false;
+                }
+
+                // just do the original function
+                return true;
             }
 
 
@@ -187,7 +265,6 @@ namespace ClaySurgeonMod
                         node.displayVideo = clayNode.displayVideo;
                         node.displayText = clayNode.displayText;
                         node.creatureName = clayNode.creatureName;
-                        Logger.LogDebug(node.displayVideo);
                         foreach (TerminalKeyword keyword in keywords)
                         {
                             if (keyword.word == "barber")
@@ -196,6 +273,7 @@ namespace ClaySurgeonMod
                                 return;
                             }
                         }
+                        Logger.LogDebug("Barber bestiary entry replaced!");
                         return;
                     }
                 }
